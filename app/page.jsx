4d68910dcd041bcd3eff18984a11eb69bc1c3e40
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-07-03 11:05:27 +0700

    wip

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
+import Hero from "@/components/Hero";
 import Link from "next/link";
 
 const HomePage = () => {
 	return (
-		<div>
-			<h1 className="text-3xl">Welcome</h1>
-			<Link href="/properties">Show Properties</Link>
-		</div>
+		<>
+			<Hero />
+		</>
 	);
 };
 
