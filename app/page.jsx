08cc1143d337e0191ef107b12fbdcd5771f0e7b0
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-07-19 15:53:05 +0700

    add featured properties section to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
-	return (
-		<>
-			<Hero />
-			<InfoBoxes />
-		</>
-	);
+    return (
+        <>
+            <Hero />
+            <InfoBoxes />
+            <HomeProperties />
+        </>
+    );
 };
 
 export default HomePage;
