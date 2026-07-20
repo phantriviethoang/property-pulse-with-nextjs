e81faebd7ee100d3112bdd0720985beaa2d4fa90
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-07-20 20:23:04 +0700

    connect mongo db with compass

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,13 @@ import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
-    return (
-        <>
-            <Hero />
-            <InfoBoxes />
-            <HomeProperties />
-        </>
-    );
+	return (
+		<>
+			<Hero />
+			<InfoBoxes />
+			<HomeProperties />
+		</>
+	);
 };
 
 export default HomePage;
